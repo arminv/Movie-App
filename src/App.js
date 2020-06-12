7a1adad8cb5aafc339b27a5b636ee4355be63c5b
arminv <arminv@users.noreply.github.com>
2020-06-12 02:23:46 -0400

    Added navbar. Initial search throttling (lodash)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Navbar from './components/Navbar'
 import Home from './containers/Home';
 import Movie from './containers/Movie';
 
@@ -9,6 +10,7 @@ import './App.css';
 const App = () => {
   return (
     <div className='App'>
+      <Navbar />
       <Switch>
         <Route exact path='/movie/:id' component={Movie}></Route>
         <Route exact path='/:myPage?' component={Home}></Route>
