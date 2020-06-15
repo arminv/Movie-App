565106c2a889d24197922fe7004aa34f4d798d2d
arminv <arminv@users.noreply.github.com>
2020-06-15 00:26:31 -0400

    Started work on the backend

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// Connect Database:
+connectDB();
+
+app.get('/', (req, res) => res.send('API Running...'));
+
+// Define Routes:
+app.use('/api/movies', require('./routes/api/movies'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
