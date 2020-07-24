55fb76a50c0bcbb760412bcdbb35ed65ddda3e6a
arminv <arminv@users.noreply.github.com>
2020-07-23 23:29:39 -0400

    Removed redux-devtools conditional (Heroku error)

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -6,8 +6,9 @@ import rootReducer from './reducers';
 const middleware = [thunk];
 
 const devTools =
-  process.env.NODE_ENV === 'production'
-    ? applyMiddleware(...middleware)
-    : composeWithDevTools(applyMiddleware(...middleware));
+  // process.env.NODE_ENV === 'production'
+  //   ? applyMiddleware(...middleware)
+  //   : composeWithDevTools(applyMiddleware(...middleware));
+  composeWithDevTools(applyMiddleware(...middleware));
 
 export default createStore(rootReducer, devTools);
