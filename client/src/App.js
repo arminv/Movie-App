c93c6f2ebd4c0a9fb7d86c4bd33e7a94038f6814
arminv <arminv@users.noreply.github.com>
2020-08-27 00:24:55 -0400

    Added route-based code splitting

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import store from './redux/store';
 import { loadUser } from './redux/actions';
 
-import Navbar from './components/Navbar';
-import Home from './containers/Home';
-import Movie from './containers/Movie';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import AlertBar from './components/AlertBar';
-import Cart from './containers/Cart';
-import PrivateRoute from './components/routing/PrivateRoute';
+import LinearProgress from '@material-ui/core/LinearProgress';
 
 import './App.css';
+
+const Navbar = lazy(() => import('./components/Navbar'));
+const Home = lazy(() => import('./containers/Home'));
+const Movie = lazy(() => import('./containers/Movie'));
+const Login = lazy(() => import('./components/auth/Login'));
+const Register = lazy(() => import('./components/auth/Register'));
+const AlertBar = lazy(() => import('./components/AlertBar'));
+const Cart = lazy(() => import('./containers/Cart'));
+const PrivateRoute = lazy(() => import('./components/routing/PrivateRoute'));
 
 const App = () => {
   useEffect(() => {
@@ -22,33 +24,35 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='App'>
-        <Navbar />
-        <AlertBar />
-        <Switch>
-          <Route exact path='/register' component={Register} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/movie/:id' component={Movie} />
-          <PrivateRoute exact path='/cart' component={Cart} />
-          <Route exact path='/:myPage?' component={Home} />
-        </Switch>
-        <p
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-            padding: '10px 0px',
-            fontSize: '16px',
-            backgroundColor: 'transparent'
-          }}
-        >
-          @2020 by{' '}
-          <a
-            href='https://www.arminvarshokar.com'
-            target='_blank'
-            rel='noopener noreferrer'
+        <Suspense fallback={<LinearProgress />}>
+          <Navbar />
+          <AlertBar />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/movie/:id' component={Movie} />
+            <PrivateRoute exact path='/cart' component={Cart} />
+            <Route exact path='/:myPage?' component={Home} />
+          </Switch>
+          <p
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+              padding: '10px 0px',
+              fontSize: '16px',
+              backgroundColor: 'transparent',
+            }}
           >
-            Armin Varshokar
-          </a>
-        </p>
+            @2020 by{' '}
+            <a
+              href='https://www.arminvarshokar.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Armin Varshokar
+            </a>
+          </p>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
