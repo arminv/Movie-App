afde01604f2a0761c2ceb676abcd3024bdd5e939
arminv <arminv@users.noreply.github.com>
2022-06-17 19:51:10 -0400

    Add '' to jwt env var reading (hack)

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,7 +15,7 @@ module.exports = function (req, res, next) {
   // Verify token:
   try {
     // const decoded = jwt.verify(token, config.get('jwtSecret'));
-    const decoded = jwt.verify(token, process.env.jwtSecret);
+    const decoded = jwt.verify(token, '' + process.env.jwtSecret);
     req.user = decoded.user;
     next();
   } catch (err) {
